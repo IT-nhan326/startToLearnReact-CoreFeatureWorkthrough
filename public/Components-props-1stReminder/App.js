35235c118ec1def51a80de76b0ddcf19fa570f58
IT-nhan326 <nhan.plt2603@gmail.com>
2021-08-18 01:11:36 +0700

    1stReminder

---
+++ public/Components-props-1stReminder/App.js
@@ -0,0 +1,15 @@
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div>
+      <h1>To Dos!!</h1>
+      <Todo text='Step 1 React'/>
+      <Todo text='Step 2 React'/>
+      <Todo text='Step 3 React'/>
+
+    </div>
+  );
+}
+
+export default App;
