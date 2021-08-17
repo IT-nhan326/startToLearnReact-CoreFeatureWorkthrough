abe6476f4da9975d930bbc357f04a9517d7974ad
IT-nhan326 <nhan.plt2603@gmail.com>
2021-08-17 18:16:16 +0700

    ReactSetup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>To Dos!!</h1>
+      <div className='card'>
+        <h2>Tittile</h2>
+        <div className='actions'>
+          <button className='btn'>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
